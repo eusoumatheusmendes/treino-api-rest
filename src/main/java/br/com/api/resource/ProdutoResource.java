a98ba831035bde72e487bf96190a6db844d8ea08
Matheus Mendes <=>
2021-11-12 18:11:13 -0300

    Disponibilizando o cadastro de produtos

--- src/main/java/br/com/api/resource/ProdutoResource.java
+++ src/main/java/br/com/api/resource/ProdutoResource.java
@@ -3,10 +3,7 @@ package br.com.api.resource;
 import br.com.api.model.Produto;
 import br.com.api.repository.IProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,5 +23,11 @@ public class ProdutoResource {
     @GetMapping("/{id}")
     public Optional<Produto> buscarPorId(@PathVariable("id") Long id){
         return repository.findById(id);
+    }
+    
+    @PostMapping("/cadastrar")
+    public Produto cadastrar(@RequestBody Produto produto){
+        repository.save(produto);
+        return produto;
     }
 }
