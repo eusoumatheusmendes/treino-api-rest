0063f2529e873085b2782fa7583c8822cd77604c
Matheus Mendes <=>
2021-11-12 16:32:14 -0300

    refatorando rotas de recursos e implementando buscar produto por id

--- src/main/java/br/com/api/resource/ProdutoResource.java
+++ src/main/java/br/com/api/resource/ProdutoResource.java
@@ -4,20 +4,27 @@ import br.com.api.model.Produto;
 import br.com.api.repository.IProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/produto")
 public class ProdutoResource {
 
     @Autowired
     IProdutoRepository repository;
 
-    @GetMapping("/produtos")
+    @GetMapping("/lista")
     public List<Produto> listar(){
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Produto> buscarPorId(@PathVariable("id") Long id){
+        return repository.findById(id);
     }
 }
